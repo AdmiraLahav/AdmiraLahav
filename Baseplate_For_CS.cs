5331bbdfcfbe824c3509a6e225349082148e772b
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-02-01 14:34:31 +0200

    Create Baseplate_For_CS.cs

---
+++ Baseplate_For_CS.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+          
+            int[] arr = new int[length];
+          
+            int num = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine($"");
+          
+        }
+    }
+}
