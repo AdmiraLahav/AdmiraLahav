363e6349a6d023d67da69ae6f206d3e1f771e1c1
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-02-02 16:28:51 +0200

    Update Baseplate_For_CS.cs

--- Baseplate_For_CS.cs
+++ Baseplate_For_CS.cs
@@ -3,9 +3,9 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.IO;
-
+//            --- This is my C# starting baseplate ---
+//            --- It is made for me cuz im lazy and copy pasting code snippets is easier ---
 //namespace Name_of_program
-//{
     class Program
     {
         static void Main(string[] args)
@@ -20,4 +20,3 @@ using System.IO;
           
         }
     }
-//}
