7949bb533e77ae4077ad2aa9b8fe3bed87587adf
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-02-02 16:29:19 +0200

    Update comment formatting in Baseplate_For_CS.cs

--- Baseplate_For_CS.cs
+++ Baseplate_For_CS.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.IO;
-//            --- This is my C# starting baseplate ---
+//                                --- This is my C# starting baseplate ---
 //            --- It is made for me cuz im lazy and copy pasting code snippets is easier ---
 //namespace Name_of_program
     class Program
